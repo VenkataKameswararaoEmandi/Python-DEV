15375ef8eb7fac67d8f72b852302777fdff0a96b
VenkataKameswararaoEmandi <110965802+VenkataKameswararaoEmandi@users.noreply.github.com>
2022-09-12 13:57:46 +0530

    first commit

--- public-lib/src/App.js
+++ public-lib/src/App.js
@@ -1,17 +1,13 @@
 import React from "react";
 
 function App() {
-<<<<<<< HEAD
   return <div className="App"></div>;
-=======
+
   return (
     <div className="App">
-      <header className="App-header">
-        Hello world
-      </header>
+      <header className="App-header">Hello world</header>
     </div>
   );
->>>>>>> fbef6d5150722d0160db8370ef9f17378f89b69b
 }
 
 export default App;
